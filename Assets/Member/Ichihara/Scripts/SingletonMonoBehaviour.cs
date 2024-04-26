06f612f67ed007af674363c6b728520ba9a8e5ff
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2024-04-26 13:01:17 +0900

    Create SingletonMonoBehaviour
    シングルトンインスタンスクラスの基底クラスを作成した。

---
+++ Assets/Member/Ichihara/Scripts/SingletonMonoBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Type t = typeof(T);
+                _instance = (T)FindObjectOfType(t);
+                if (_instance == null)
+                {
+#if UNITY_EDITOR
+                    Debug.LogError($"{t} をアタッチしているゲームオブジェクトはありません。");
+#endif
+                }
+            }
+            return _instance;
+        }
+    }
+
+    /// <summary>
+    /// 継承先のクラスから base.Awake() で呼び出す
+    /// </summary>
+    protected void Awake()
+    {
+        // 他のゲームオブジェクトにアタッチされているかを調べる。
+        // アタッチされている場合は破棄する。
+        if (this != Instance)
+        {
+            Destroy(this);
+#if UNITY_EDITOR
+            Debug.LogWarning(
+                $"{typeof(T)} は既に他のゲームオブジェクトにアタッチされている為、コンポーネントを破棄しました。" +
+                $"現在アタッチされているゲームオブジェクトは、{Instance.gameObject.name} です。");
+#endif
+            return;
+        }
+    }
+}
