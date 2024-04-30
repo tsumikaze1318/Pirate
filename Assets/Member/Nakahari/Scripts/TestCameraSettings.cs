47149bbd21f22d00b84aa2121d4ccd94adc38739
Shota Nakahari <fire.stnr@icloud.com>
2024-05-01 02:24:23 +0900

    頭こんがらがってきた

--- Assets/Member/Nakahari/Scripts/TestCameraSettings.cs
+++ Assets/Member/Nakahari/Scripts/TestCameraSettings.cs
@@ -7,6 +7,19 @@ public class TestCameraSettings : MonoBehaviour
 {
     private Camera _camera;
     private PlayerInput _playerInput;
+
+    private Vector2 _look = Vector2.zero;
+
+    public float _minCameraAngle = -45;
+    public float _maxCameraAngle = 75;
+
+    private void OnLook(InputValue value)
+    {
+        // LookAction ‚Ì“ü—Í’l‚ÌŽæ“¾
+        var axis = value.Get<Vector2>();
+        _look = new Vector2(axis.x, axis.y);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +27,10 @@ public class TestCameraSettings : MonoBehaviour
         _playerInput = GetComponentInParent<PlayerInput>();
 
         _camera.targetDisplay = _playerInput.user.index;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(new Vector3(_look.y, -_look.x, 0f));
     }
 }
--- Assets/Member/Nakahari/Scripts/TestInputSystem.cs
+++ Assets/Member/Nakahari/Scripts/TestInputSystem.cs
@@ -14,7 +14,7 @@ public class TestInputSystem : MonoBehaviour
     public CommonParam.UnitState _state = CommonParam.UnitState.Normal;
 
     private Vector3 _move;
-    private Vector3 _look;
+    
 
     private Camera _camera;
     [SerializeField]
@@ -36,6 +36,9 @@ public class TestInputSystem : MonoBehaviour
     [SerializeField]
     float _upForce;
 
+    private Vector3 _prevPosition;
+    private Transform _transform;
+
     #endregion
 
     private void OnMove(InputValue value)
@@ -44,13 +47,6 @@ public class TestInputSystem : MonoBehaviour
         var axis = value.Get<Vector2>();
         // 移動速度の保持
         _move = new Vector3(axis.x, 0, axis.y) * _moveSpeed;
-    }
-
-    private void OnLook(InputValue value)
-    {
-        // LookAction の入力値の取得
-        var axis = value.Get<Vector2>();
-        _look = new Vector3(axis.x, axis.y, 0);
     }
 
     private void OnFire(InputValue value)
@@ -106,6 +102,20 @@ public class TestInputSystem : MonoBehaviour
         }
     }
 
+    private void PlayerLookDirection()
+    {
+        var position = _transform.position;
+        var delta = position - _prevPosition;
+
+        _prevPosition = position;
+
+        if (delta == Vector3.zero) return;
+
+        var rotation = Quaternion.LookRotation(delta, Vector3.up);
+
+        _transform.rotation = rotation;
+    }
+
     public void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
@@ -119,14 +129,22 @@ public class TestInputSystem : MonoBehaviour
         _targetObject = this.gameObject;
         if(_camera == null) _camera = GetComponentInChildren<Camera>();
         if (_rb == null) _rb = GetComponent<Rigidbody>();
+
+        _transform = transform;
+        _prevPosition = _transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // 移動座標を加算する
-        transform.localPosition += _move;
+        PlayerLookDirection();
         //_camera.transform.RotateAround(_targetObject.transform.position, new Vector3(_look.y, _look.x, 0), _lookSpeed);
         //_camera.transform.localEulerAngles += _look;
+    }
+
+    private void FixedUpdate()
+    {
+        // 移動座標を加算する
+        transform.localPosition += _move;
     }
 }
