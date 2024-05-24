a42e5559519a37c1baf7657ea8eea489975ac29c
YusakuYokota <ttc22c1020051@edu.tech.ac.jp>
2024-05-24 15:11:52 +0900

    同点処理解決

--- Assets/Member/Yokota/Scripts/ShowRanking.cs
+++ Assets/Member/Yokota/Scripts/ShowRanking.cs
@@ -6,11 +6,14 @@ public class ShowRanking : MonoBehaviour
 {
     private void Start()
     {
-        for (int i = 0; i < GameManager.ScoreRanking.Length; i++)
+        for (int i = 0; i < GameManager.ScoreToPlayer.Count; i++)
         {
-            Instantiate(GameManager.ScoreToPlayer[GameManager.ScoreRanking[i]]
+            for (int j = 0; j < GameManager.ScoreToPlayer.Count; j++)
+            {
+                Instantiate(GameManager.ScoreToPlayer[GameManager.ScoreRanking[i]][j]
                         , new Vector3(i * 2, 0, 0)
                         , Quaternion.identity);
+            }
         }
     }
 }
