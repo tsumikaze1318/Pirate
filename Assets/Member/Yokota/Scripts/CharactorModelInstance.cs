26205ec990db32e1c2cef41e8b3798753c3cbcfa
YusakuYokota <ttc22c1020051@edu.tech.ac.jp>
2024-06-14 11:23:26 +0900

    同点処理の不具合解決

--- Assets/Member/Yokota/Scripts/CharactorModelInstance.cs
+++ Assets/Member/Yokota/Scripts/CharactorModelInstance.cs
@@ -11,13 +11,25 @@ public class CharactorModelInstance : MonoBehaviour
 
     private void ModelInstance()
     {
+        int count = 0;
+
+        List<int> scores = new List<int>();
+
+        foreach (var ranking in GameManager.ScoreToPlayer)
+        {
+            var key = ranking.Key;
+            scores.Add(key);
+        }
+
         for (int i = 0; i < GameManager.ScoreToPlayer.Count; i++)
         {
-            for (int j = 0; j < GameManager.ScoreToPlayer[GameManager.ScoreRanking[i]].Count; j++)
+            for (int j = 0; j < GameManager.ScoreToPlayer[scores[i]].Count; j++)
             {
-                var plObj = Instantiate(GameManager.ScoreToPlayer[GameManager.ScoreRanking[i]][j]
-                        , new Vector3(i * 2 - 3, 0, 0)
+                var plObj = Instantiate(GameManager.ScoreToPlayer[scores[i]][j]
+                        , new Vector3(count * 2 - 3, 0, 0)
                         , Quaternion.identity);
+
+                count++;
             }
         }
     }
